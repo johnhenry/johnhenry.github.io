f1d4254ac34843e574d86103335321f4e5736418
(none) <john@iamjohnhenry.com>
2021-10-25 10:48:15 -0700

    fix header spacing and some chores

--- src/external/styles.mjs
+++ src/external/styles.mjs
@@ -4,5 +4,7 @@ export default [
   `${SITE_LIB_URL}vendor/css/reset/2.0.0/index.css`,
   `${SITE_LIB_URL}css/universal-unstyled-links/0.0.0/index.css`,
   `${SITE_LIB_URL}css/universal-box-sizing/0.0.0/index.css`,
+  `${SITE_LIB_URL}css/universal-no-margins/0.0.0/index.css`,
   `/style/index.css`,
+  `${SITE_LIB_URL}css/hide-n-show/0.0.0/landscape.css`,
 ];
