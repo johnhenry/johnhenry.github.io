fbe207d567d24b87e51394183b81a7b65df6ff6f
(none) <john@iamjohnhenry.com>
2021-10-27 09:25:34 -0700

    Add more cats

--- src/content/projects.mjs
+++ src/content/projects.mjs
@@ -1,35 +1,34 @@
 import { SITE_LIB_URL, SITE_BASE_PATH } from "../SETTINGS.mjs";
 const image = `${SITE_BASE_PATH}vendor/img/www.pexels.com/pixabay/turned-on-computer-monitor-displaying-text-300.jpg`;
-const image2 = `${SITE_BASE_PATH}personal/image/bufka.karolina/cat-in-sun.jpg`;
 export default [
   {
     title: "Bash Command Line tools",
     description: "A collection of tools that I find useful.",
     href: "https://johnhenry.github.io/lib/bash",
-    image,
+    image: `${SITE_BASE_PATH}personal/image/bufka.karolina/cat-in-sun.jpg`,
   },
   {
     title: "Standard Library -- set of components for building applications.",
     description: "Click here",
     href: SITE_LIB_URL,
-    image: image2,
+    image,
   },
   {
     title: "Forsnaken -- Multiplayer snake game",
     description: "A modular snake game",
     href: "https://johnhenry.github.io/forsnaken",
-    image,
+    image: `${SITE_BASE_PATH}personal/image/bufka.karolina/cat-in-towel.jpg`,
   },
   {
     title: "HTML Builder",
     description: "Build HTML with components",
     href: "https://johnhenry.github.io/htmlbuilder",
-    image: image2,
+    image,
   },
   {
     title: "Blog",
     description: "Thoughts and musings",
     href: "https://johnhenry.github.io/blog",
-    image,
+    image: `${SITE_BASE_PATH}personal/image/bufka.karolina/cat-on-chair.jpg`,
   },
 ];
