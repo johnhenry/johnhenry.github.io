ef7e2d838b3c0e3fee503bb133a840cbf7f6040b
John <john@iamjohnhenry.com>
2022-04-11 22:45:18 -0700

    hot-fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
-import solid from "@astrojs/solid-js";
 // @ts-check
 
 /** @type {import('astro').AstroUserConfig} */
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [solid()],
+  integrations: [],
   vite: {
-    plugins: []
-  }
+    plugins: [],
+  },
 });
--- src/HASH.mjs
+++ src/HASH.mjs
@@ -1,1 +1,1 @@
-export default "e87a09b5547920f509ff5e8cbf5d47d4f5425723";
+export default "";
