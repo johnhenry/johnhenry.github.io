7078b43fa7a21515374e29f9d8ab5f94765697fc
John <john@iamjohnhenry.com>
2022-04-11 22:30:42 -0700

    add animate-paths component

--- src/external/scripts.mjs
+++ src/external/scripts.mjs
@@ -3,4 +3,6 @@ import { SITE_LIB_URL } from "../SETTINGS.mjs";
 export default [
   `${SITE_LIB_URL}js/css-variables-mouse/0.0.0/index.mjs`,
   `${SITE_LIB_URL}js/css-variables-scroll/0.0.0/index.mjs`,
+  `${SITE_LIB_URL}js/until-window-load/0.0.0/global.mjs`,
+  `${SITE_LIB_URL}js/define-component.component/0.0.0/global.mjs`,
 ];
