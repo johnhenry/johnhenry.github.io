740dc1fd0682004ae35b479c2e2a9167c271bcb5
John <john@iamjohnhenry.com>
2022-05-03 16:34:32 -0700

    move class cycler to web component

--- src/external/components.mjs
+++ src/external/components.mjs
@@ -4,4 +4,5 @@ export default [
     "animate-paths",
     `${SITE_LIB_URL}js/animate-paths.component/0.0.0/index.mjs`,
   ],
+  ["class-cycler", `${SITE_LIB_URL}js/class-cycler.component/0.0.0/index.mjs`],
 ];
